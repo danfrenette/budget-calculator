31826afa4062763415490c886090eb0380a6dc06
danfrenette <dan.r.frenette@gmail.com>
2020-02-29 17:33:17 -0500

    Add Income input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,40 @@ import React from 'react';
 import Bucket from './Bucket';
 import './App.css';
 import { Container } from 'reactstrap';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Col,
+  Row,
+  Label,
+  Input
+} from 'reactstrap';
+
+const Income = () => {
+  return (
+    <Row>
+      <Col md="2">
+        <Label>Income</Label>
+      </Col>
+      <Col md="5">
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>$</InputGroupText>
+          </InputGroupAddon>
+          <Input
+            className="form-control"
+            type="number"
+            defaultValue={0}
+          />
+        </InputGroup>
+      </Col>
+      <Col md="2">
+        <span>$0.00</span>
+      </Col>
+    </Row>
+  )
+}
 
 class App extends React.Component {
   renderBuckets = () => {
@@ -23,6 +57,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Container>
+          <Income/>
           { this.renderBuckets() }
         </Container>
       </div>
